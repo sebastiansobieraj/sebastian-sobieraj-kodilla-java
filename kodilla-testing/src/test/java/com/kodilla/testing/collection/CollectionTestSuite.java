291c21cfc7e5c84c116dc44934bd1522fb7c439c
sebastiansobieraj <sebastian.sobieraj91@gmail.com>
2017-12-06 19:04:31 +0100

    Exercise 6.3 - add before and after method

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,11 +1,23 @@
 package com.kodilla.testing.collection;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
 public class CollectionTestSuite {
+
+    @Before
+    public void before (){
+        System.out.println("Test Case: begin");
+    }
+
+    @After
+    public void after (){
+        System.out.println("Test Case: end");
+    }
 
     @Test
     public void testOddNumbersExterminatorEmptyList() {
