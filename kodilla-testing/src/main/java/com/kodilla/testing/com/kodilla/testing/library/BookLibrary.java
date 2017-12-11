2f76bef7a02f22d50581e5181743f13afa715fc7
sebastiansobieraj <sebastian.sobieraj91@gmail.com>
2017-12-11 13:47:44 +0100

    Exercise 6.6 - v2

--- kodilla-testing/src/main/java/com/kodilla/testing/com/kodilla/testing/library/BookLibrary.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/com/kodilla/testing/library/BookLibrary.java
@@ -4,31 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookLibrary {
-    LibraryDatabase libraryDatabase;
+    private LibraryDatabase libraryDatabase;
 
     public BookLibrary(LibraryDatabase libraryDatabase) {
         this.libraryDatabase = libraryDatabase;
     }
 
     public List<Book> listBooksWithCondition(String titleFragment) {
-        List<Book> bookList = new ArrayList<Book>();
-        if (titleFragment.length() < 3) return bookList;
+        if (titleFragment.length() < 3) return new ArrayList<>();
         List<Book> resultList = libraryDatabase
                 .listBooksWithCondition(titleFragment);
-        if (resultList.size() > 20) return bookList;
-        bookList = resultList;
-        return bookList;
+        if (resultList.size() > 20) return new ArrayList<>();
+        return resultList;
     }
 
     public List<Book> listBooksInHandsOf(LibraryUser libraryUser) {
-        List<Book> bookList = new ArrayList<Book>();
         if (libraryDatabase.listBooksInHandsOf(libraryUser).size() <= 0){
-            return bookList;
+            return new ArrayList<>();
         }
         List<Book> userBookList = libraryDatabase.listBooksInHandsOf(libraryUser);
         if(userBookList.size() > 0) {
-            bookList = userBookList;
         }
-        return bookList;
+        return userBookList;
     }
 }
