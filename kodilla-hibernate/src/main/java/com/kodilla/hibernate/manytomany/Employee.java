eec16def883da8dcf256abef924a54a0cf896298
sebastiansobieraj <sebastian.sobieraj91@gmail.com>
2018-03-27 20:01:49 +0200

    Facade for Hibernate with corrects

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Employee.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Employee.java
@@ -13,7 +13,7 @@ import java.util.List;
 
         @NamedQuery(
                 name = "Employee.searchByPartOfName",
-                query = "FROM Employee WHERE lastname = :CHARACTERS"
+                query = "FROM Employee WHERE lastname LIKE :CHARACTERS"
         )
 })
 
